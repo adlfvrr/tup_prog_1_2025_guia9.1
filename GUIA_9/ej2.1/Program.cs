3b045840ff223a600598f0ce55b4915919fcd8ca
adlfvrr <adolfover4@gmail.com>
2025-06-15 17:29:29 -0300

    -

---
+++ GUIA_9/ej2.1/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace ej2._1
+{
+    internal class Program
+    {
+        static void Intercambio(int[] arreglo, int i, int j)
+        {
+            int aux = arreglo[i];
+            arreglo[i] = arreglo[j];
+            arreglo[j] = aux;
+        }
+        static void Burbuja(int[] arreglo)
+        {
+            for (int j = 0; j < (arreglo.Length - 1); j++)
+            {
+                for (int k = j + 1; k < arreglo.Length; k++)
+                {
+                    if (arreglo[j] < arreglo[k])
+                    {
+                        Intercambio(arreglo, j, k);
+                    }
+                }
+                if (arreglo[j] != 0)
+                {
+                    Console.Write($"{j}:{arreglo[j]}, ");
+                }
+            }
+        }
+        static int Particion(int[] arreglo, int iMenor, int iMayor)
+        {
+            int pivot = arreglo[iMayor];
+            int j = iMenor - 1;
+            for (int i = iMenor; i < iMayor; i++)
+            {
+                if (arreglo[i] <= pivot)
+                {
+                    j++;
+                }
+            }
+            Intercambio(arreglo, j + 1, iMayor);
+            return j + 1;
+        }
+        static void QuickSort(int[] arreglo, int iMenor, int iMayor)
+        {
+            if (iMenor < iMayor)
+            {
+                int pivot = Particion(arreglo, iMenor, iMayor);
+                QuickSort(arreglo, iMenor, pivot - 1);
+                QuickSort(arreglo, pivot + 1, iMayor);
+            }
+        }
+        static int Busqueda(int[] arreglo, int valor)
+        {
+            int encontrado = -1;
+            for (int i = 0; i < arreglo.Length - 1; i++)
+            {
+                if (arreglo[i] == valor)
+                {
+                    encontrado = i;
+                }
+            }
+            return encontrado;
+        }
+        static void Main(string[] args)
+        {
+            int[] arreglo = new int[100];
+            int[] arregloBurbuja = new int[20];
+            int[] arregloQuickSort = new int[20];
+            Random elementosVector = new Random();
+            for (int i = 0; i < 20 - 1; i++)
+            {
+                arreglo[i] = elementosVector.Next(200);
+                Console.Write($"{i}:{arreglo[i]}, ");
+                arregloBurbuja[i] = arreglo[i];
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            //Ordenamiento Burbuja
+            Burbuja(arregloBurbuja);
+            for (int i = 0; i < arregloBurbuja.Length - 1; i++)
+            {
+                arregloQuickSort[i] = arregloBurbuja[i];
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            //Ordenamiento QuickSort
+            QuickSort(arregloQuickSort, 0, 19);
+            for (int i = 0; i < arregloQuickSort.Length - 1; i++)
+            {
+                if (arregloQuickSort[i] != 0)
+                    Console.Write($"{i}:{arregloQuickSort[i]}, ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            //Búsqueda de un elemento
+            int valorAleatorio = elementosVector.Next(1,200);
+            int indiceAEncontrar = Busqueda(arregloQuickSort, valorAleatorio);
+            if (indiceAEncontrar != -1)
+            {
+                Console.WriteLine($"El numero {valorAleatorio} se encuentra en el índice: {indiceAEncontrar} dentro de la lista ordenada por QuickSort.");
+            }
+            else
+            {
+                Console.WriteLine($"{valorAleatorio} no encontrado.");
+            }
+        }
+    }
+}
---
+++ GUIA_9/prácticas_metodos_ord/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace prácticas_metodos_ord
+{
+    internal class Program
+    {
+        static void Intercambio(int[]arreglo, int indPrimero, int indSegundo)
+        {
+            int aux = arreglo[indPrimero];
+            arreglo[indPrimero] = arreglo[indSegundo];
+            arreglo[indSegundo] = aux;
+        }
+        static void Burbuja(int[] arreglo)
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                for (int j = i + 1; j < arreglo.Length; j++)
+                {
+                    if (arreglo[i] > arreglo[j])
+                    {
+                        Intercambio(arreglo, i, j);
+                    }
+                }
+                Console.Write($"{i}:{arreglo[i]}, ");
+            }
+        }
+        static int Particion(int[] arreglo,int indMenor,int indMayor)
+        {
+            int pivot = arreglo[indMayor];
+            int j = indMenor - 1;
+            for(int i = indMenor; i < indMayor; i++)
+            {
+                if (arreglo[i] >= pivot)
+                {
+                    j++;
+                }
+            }
+            Intercambio(arreglo, j + 1, indMayor);
+            return j + 1;
+        }
+        static void QuickSort(int[] arreglo, int indMenor, int indMayor)
+        {
+            if (indMenor < indMayor)
+            {
+                int pivot = Particion(arreglo, indMenor, indMayor);
+                QuickSort(arreglo, indMenor, pivot - 1);
+                QuickSort(arreglo, pivot + 1, indMayor);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //METODOS ORDENAMIENTO
+            int[] arreglo = new int[5];
+            arreglo = [3, 8, 5, 7, 2];
+            //Metodo Burbuja
+            Burbuja(arreglo);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            //Metodo QuickSort
+            QuickSort(arreglo, 0, 4);
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write($"{i}:{arreglo[i]}, ");
+            }
+
+        }
+    }
+}
